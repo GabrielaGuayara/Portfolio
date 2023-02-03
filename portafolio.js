d81314d52b1010fae5e119f82d2e910175754f22
Gabriela <gabriela.guayara@gmail.com>
2023-02-03 11:08:01 -0500

    first commit

---
+++ portafolio.js
@@ -0,0 +1,41 @@
+/*--------Projects Section Funtionality-------------*/
+
+var tablinks = document.getElementsByClassName("links");
+var tabcontents = document.getElementsByClassName("contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0"
+}
+function closemenu(){
+    sidemenu.style.right = "-200px"
+}
+
+/*------------------Contact me Form Funtionality---------------------- */
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzwPdhCtj9LYX0ejyPCrWyzZueyp4HVxHUDy6vvcYPcnpfFAGZRVUHLTCKrgqovSg6VRA/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML="Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML=" "
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
